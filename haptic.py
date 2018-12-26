b0a9de0690f57ae0687889d6cc0f346144991dea
Jeff Shelley <mr.jeff.shelley@gmail.com>
2018-12-26 15:56:50 -0600

    initial working version

--- haptic.py
+++ haptic.py
@@ -2,6 +2,7 @@ import os
 import sys
 import socket
 import urllib
+import math
 import json
 import csv
 from pprint import pprint
@@ -10,16 +11,20 @@ import time
 
 conf_file="haptic.conf"
 
-global config
 config={}
 last_loaded={}
+
 
 
 files_i_watch_for = {}
 
 
+
 def load_file(s):
 	return open(s).read()
+
+
+
 
 
 
@@ -28,7 +33,7 @@ def load_csv(s):
 		r = csv.reader(csvfile)
 		response={}
 		for row in r:
-			response[row[0]]={"start": row[1], 'duration': row[2]}
+			response[row[0]]={"start": float(row[1]), 'duration': map(float, row[2:])}
 		return response
 
 
@@ -45,7 +50,7 @@ def need_to_reload (s):
 	    last_modified_date = os.path.getmtime(s)
 	    return (last_modified_date > ( (last_loaded[s] if s in last_loaded else -1) + 5 ))
 	else:
-	    print( "couldn't find file: %s" (s))
+	    print( "couldn't find file: %s" % (s))
 
 
 
@@ -86,9 +91,7 @@ def parse_buzzers():
 
 
 def wait ():
-	time.sleep(3)
-
-
+	time.sleep(config["sleep-time-in-seconds"])
 
 
 def load_files ():
@@ -104,12 +107,82 @@ def fetch_json_url (url):
 	
 
 
-def process_anything_running ():
+def when_playing (f):
 	player_data=fetch_json_url( config["player-data-url"])
 	if "file" in player_data and player_data["file"] in files_i_watch_for:
-		position=player_data["position_millis"] / 1000;
-		pprint(position)
-	# pprint(player_data)
+		f(player_data)
+
+
+
+
+def figure_future_buzz (player_data):
+	where_we_are = player_data["position_millis"]/1000.0
+	started_playing_at = time.time()-where_we_are
+	buzz_point_in_video = files_i_watch_for[player_data["file"]]["start"] / 1000.0
+	return started_playing_at + buzz_point_in_video 
+
+
+
+def buzz (duration):
+	print("buzzing for %s" % (duration / 1000.0))
+	time.sleep(duration / 1000.0)
+	
+
+
+def do_buzzes(durations):
+	# print(durations)
+	for i in range(0, int(math.ceil(len(durations) / 2.0))):
+		buzz(durations[i*2])
+		if 1+i*2 < len(durations):
+			print("waiting for %s" % (durations[i*2 + 1]/ 1000.0 ))
+			time.sleep(durations[i*2 + 1] / 1000.0)
+	print("done")
+
+
+def execute_buzz(player_data):
+	buzz_at = figure_future_buzz(player_data)
+	time_till_buzz = buzz_at - time.time()
+
+	durations = files_i_watch_for[player_data["file"]]["duration"]
+
+
+	if time_till_buzz > 0.0:
+		
+		# the buzz is in the future
+		print("will buzz in %s" % (time_till_buzz))
+
+		#sleep until it's time to buzz
+		time.sleep(time_till_buzz)
+
+		do_buzzes(durations)
+
+
+
+
+
+
+
+def just_found_file(player_data):
+	buzz_at = figure_future_buzz(player_data)
+	time_till_buzz = buzz_at - time.time()
+
+	if time_till_buzz > 0.0:
+		# the buzz is in the future
+		print("will buzz in %s" % (time_till_buzz))
+
+		print("durations will be %s" % (files_i_watch_for[player_data["file"]]["duration"]))
+
+
+		#sleep until we're 10 seconds out
+		time.sleep(time_till_buzz - 10)
+
+		when_playing(execute_buzz)
+
+
+
+
+def process_anything_running ():
+	when_playing(just_found_file)
 
 
 def runit ():
